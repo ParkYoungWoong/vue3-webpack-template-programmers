5d1093e569e32593ecea98b4c3b00f361ec2ca8d
ParkYoungWoong <thesecon@gmail.com>
2021-09-09 18:55:29 +0900

    Create project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <Hello />
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+
+export default {
+  components: {
+    Hello
+  },
+  data() {
+    return {
+      msg: 'Hello Webpack?..'
+    }
+  }
+}
+</script>
