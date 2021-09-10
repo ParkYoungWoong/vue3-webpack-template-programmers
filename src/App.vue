90131636cf546139c6fcb7a623a7d4c079dff73a
ParkYoungWoong <thesecon@gmail.com>
2021-09-10 09:55:32 +0900

    Add eslint

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>
+    {{ msg }}
+  </h1>
   <Hello />
 </template>
 
 <script>
-import Hello from '~/components/Hello'
-
+import Hello from '~/components/Hello' 
+   
 export default {
   components: {
     Hello
   },
   data() {
     return {
-      msg: 'Hello Webpack?..'
+      msg: 'Hello Vue!'
     }
   }
 }
