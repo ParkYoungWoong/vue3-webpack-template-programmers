00c1e7d9cf6fe03f1c6087a96cf623dd4431e6a3
Leon-Miller <bagleon43@gmail.com>
2021-09-20 21:16:00 +0900

    Add router

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     clean: true
   },
   module: {
@@ -41,5 +42,8 @@ module.exports = {
         { from: 'static' }
       ]
     })
-  ]
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
 }
